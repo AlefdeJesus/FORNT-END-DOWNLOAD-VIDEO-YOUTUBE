40121bc2fd0ab2c9aaa6ffc30bdb1d0fb800529e
Alefdejesus <alefjesus1996st.s@gmail.com>
2024-07-20 16:10:05 -0300

    alteração ngrok

--- scripts/linkAPI.js
+++ scripts/linkAPI.js
@@ -1,4 +1,4 @@
-const LINK_API = 'https://b19a-45-225-120-218.ngrok-free.app';
+const LINK_API = 'https://30e8-45-225-120-218.ngrok-free.app';
 //const LINK_API = 'http://localhost:3000';
 
 export default LINK_API;
