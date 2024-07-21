f339ed6cc291053052686bcf841bed180bd278ee
Alefdejesus <alefjesus1996st.s@gmail.com>
2024-07-21 10:09:13 -0300

    alterar link back end

--- scripts/linkAPI.js
+++ scripts/linkAPI.js
@@ -1,4 +1,4 @@
-const LINK_API = 'https://30e8-45-225-120-218.ngrok-free.app';
+const LINK_API = 'https://2a57-45-225-120-218.ngrok-free.app';
 //const LINK_API = 'http://localhost:3000';
 
 export default LINK_API;
