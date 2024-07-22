496f08bdc9f890ce52ed38053281ffeaa22e2e26
Alefdejesus <alefjesus1996st.s@gmail.com>
2024-07-21 22:16:08 -0300

    retirei mensagem de erro no server

--- scripts/linkAPI.js
+++ scripts/linkAPI.js
@@ -1,4 +1,4 @@
 const LINK_API = 'https://23965d1a72db511a6c209040277e69b8.serveo.net';
-//const LINK_API = 'http://192.168.1.5:3333';
+//const LINK_API = 'http://localhost:3333';
 
 export default LINK_API;
